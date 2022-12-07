793c6d6271cab88de178b1cbf2e31bcd1168a297
suxin <837081248@qq.com>
2022-12-08 01:58:38 +0800

    填充图片

--- pages/redPacket/redPacket.js
+++ pages/redPacket/redPacket.js
@@ -8,7 +8,9 @@ Page({
             "type": 1,
             "appId": "wxde8ac0a21135c07d",
             "skip2Url": "/index/pages/h5/h5?f_userId=1&f_token=1&s_cps=1&weburl=https%3A%2F%2Fdpurl.cn%2FU6sdbbKz",
-            "extra": "额外内容111"
+            "extra": "点击领取",
+            "content": "每月的8日、18日均有大额红包，记得领取哟~",
+            "footer": "具体红包金额以实际为主"
         }, {
             "id": 2,
             "title": "饿了么红包",
@@ -16,7 +18,9 @@ Page({
             "type": 1,
             "appId": "wxece3a9a4c82f58c9",
             "skip2Url": "commercialize/pages/taoke-guide/index?scene=e9259b417247483e8f57fbdb3679a25d",
-            "extra": "额外内容222"
+            "extra": "点击领取",
+            "content": "每月的8日、18日均有大额红包，记得领取哟~",
+            "footer": "具体红包金额以实际为主"
         }, {
             "id": 3,
             "title": "滴滴红包",
@@ -24,7 +28,39 @@ Page({
             "type": 1,
             "appId": "wxaf35009675aa0b2a",
             "skip2Url": "/pages/index/index?scene=2x66qE7&source_id=ad9592ac709c14f9acfb",
-            "extra": "额外内容333"
+            "extra": "点击领取",
+            "content": "每月的8日、18日均有大额红包，记得领取哟~",
+            "footer": "具体红包金额以实际为主"
+        },{
+            "id": 4,
+            "title": "KFC福利点餐",
+            "icon": "https://cps-nj.oss-cn-nanjing.aliyuncs.com/KFC.png",
+            "type": 1,
+            "appId": "wxaf35009675aa0b2a",
+            "skip2Url": "/pages/index/index?scene=2x66qE7&source_id=ad9592ac709c14f9acfb",
+            "extra": "点击领取",
+            "content": "每月的8日、18日均有大额红包，记得领取哟~",
+            "footer": "具体红包金额以实际为主"
+        },{
+            "id": 5,
+            "title": "花小猪打车",
+            "icon": "https://cps-nj.oss-cn-nanjing.aliyuncs.com/%E8%8A%B1%E5%B0%8F%E7%8C%AA.jpg",
+            "type": 1,
+            "appId": "wxaf35009675aa0b2a",
+            "skip2Url": "/pages/index/index?scene=2x66qE7&source_id=ad9592ac709c14f9acfb",
+            "extra": "点击领取",
+            "content": "每月的8日、18日均有大额红包，记得领取哟~",
+            "footer": "具体红包金额以实际为主"
+        },{
+            "id": 6,
+            "title": "汉堡王福利点餐",
+            "icon": "https://cps-nj.oss-cn-nanjing.aliyuncs.com/%E6%B1%89%E5%A0%A1%E7%8E%8B.jpg",
+            "type": 1,
+            "appId": "wxaf35009675aa0b2a",
+            "skip2Url": "/pages/index/index?scene=2x66qE7&source_id=ad9592ac709c14f9acfb",
+            "extra": "点击领取",
+            "content": "每月的8日、18日均有大额红包，记得领取哟~",
+            "footer": "具体红包金额以实际为主"
         }]
     },
     //跳转其他小程序
